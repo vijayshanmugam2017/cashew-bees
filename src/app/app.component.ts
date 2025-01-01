f132cbff00b32e6697461827ccd6b22c72a0e6d4
Vijay Shanmugam <Vijay.Shanmugam@excelenciaconsulting.com>
2025-01-01 15:45:51 +0530

    [Refactor] - added common footer for all pages.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HeaderComponent } from './shared/header.component';
+import { FooterComponent } from './shared/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule,RouterOutlet, MainPageComponent, HeaderComponent],
+  imports: [RouterModule,RouterOutlet, MainPageComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
